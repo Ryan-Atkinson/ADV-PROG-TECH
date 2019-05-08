dc4d97c11136abe9b3436f0c41ea76a2fa4f9b7b
duc246810 <s3656289@student.rmit.edu.au>
2019-05-08 16:15:36 +1000

    New test cases

--- Assignment 2/starter_code/LinkedList.h
+++ Assignment 2/starter_code/LinkedList.h
@@ -10,6 +10,19 @@ public:
    LinkedList();
    ~LinkedList();
 
+   int size();
+   void clear();
+   int get(int i);
+
+   void addFront(Tile* tile);
+   void addBack(Tile* tile);
+
+   void deleteFront();
+   void deleteBack();
+
+   void deleteAt(int i);
+
+
 private:
    Node* head;
 };
