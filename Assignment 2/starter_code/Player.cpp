eb4a95f209a933de9f03614d0665fdd65a541dae
s3722194 <s3722194@student.rmit.edu.au>
2019-05-19 23:34:30 +1000

    updated and started implementing the player class

--- Assignment 2/starter_code/Player.cpp
+++ Assignment 2/starter_code/Player.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 #include "Player.h"
 
-// Player(playerName) {
-//   playerName = playerName;
-//   points = 0;
-// }
+Player::Player(std::string playerName) {
+  this->playerName = playerName;
+  this->points = 0;
+  this->hand= new LinkedList();
+}
+
+
+Player::~Player(){
+  delete this->hand;
+
+}
+
+
+int Player::getPoints(){
+  return this->points;
+
+}
+
+//sets the total points
+void Player::setPoints(int points){
+  this->points =points;
+}
+
+// adds to the current points and then returns the total points
+int Player::addPoints(int points){
+  this->points+=points;
+  return this->points;
+}
+
+//gets current size of hand
+int Player::getHandSize(){
+    return hand->size();
+}
+
+//gets a linked list of the current hand
+LinkedList* Player::getHand(){
+  return hand;
+}
+
+//prints a list of the current hand
+void Player::listHand(){
+  int index=0;
+  while(index<this->hand->size()){
+    Tile* tile = this->hand->get(index);
+    std::cout <<tile->colour<< ", "<< tile->shape<< " ";
+    index++;
+  }
+  std::cout<<std::endl;
+}
+
+//adds a tile to hand
+void Player::addTile(Tile* tile){
+  this->hand->addBack(tile);
+}
+
+// removes a tile for the hand
+void Player::removeTile(int index){
+  this->hand->deleteAt(index);
+
+}
--- Assignment 2/starter_code/Player.h
+++ Assignment 2/starter_code/Player.h
@@ -7,15 +7,36 @@
 
 class Player {
   private:
-    // string playerName;
-    // int points;
-    // LinkedList<char> hand;
+    std::string playerName;
+    int points;
+    LinkedList* hand;
   public:
-    // Player();
-    // int getPoints();
-    // int setPoints(int);
-    // int getHandSize();
-    // void listHand();
+    Player(std::string playerName);
+    ~Player();
+
+
+    int getPoints();
+
+    //sets the total points
+    void setPoints(int points);
+
+    // adds to the current points and then returns the total points
+    int addPoints(int points);
+
+    //gets current size of hand
+    int getHandSize();
+
+    //gets a linked list of the current hand
+    LinkedList* getHand();
+
+    //prints a list of the current hand
+    void listHand();
+
+    //adds a tile to hand
+    void addTile(Tile* tile);
+
+    // removes a tile for the hand by index of the tile in the linked list
+    void removeTile(int index);
 
 };
 
